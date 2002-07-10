45c6a2c86326672a07db3cf1d80e353994c95d5a
steveman <steveman>
2002-07-10 20:51:21 +0000

    stuff

--- config.h
+++ config.h
@@ -5,75 +5,40 @@
 #define CURSES_HDR <ncurses.h>
 
 /* Where is the curses header file ? */
-/* #undef HAVE_TEXTMODE_X11 */
+#define HAVE_TEXTMODE_X11 1
 
+/* Define to empty if the keyword does not work.  */
+/* #undef const */
 
-/* Define if you have the `acosh' function. */
-/* #undef HAVE_ACOSH */
-
-/* Define if you have the `asinh' function. */
-/* #undef HAVE_ASINH */
-
-/* Define if you have the `atanh' function. */
-/* #undef HAVE_ATANH */
-
-/* Define if you have the <curses.h> header file. */
-/* #undef HAVE_CURSES_H */
-
-/* Define if you have the <inttypes.h> header file. */
-#define HAVE_INTTYPES_H 1
-
-/* Define if you have the <memory.h> header file. */
-#define HAVE_MEMORY_H 1
-
-/* Define if you have the <ncurses.h> header file. */
-#define HAVE_NCURSES_H 1
-
-/* Define if you have the <stdint.h> header file. */
-#define HAVE_STDINT_H 1
-
-/* Define if you have the <stdlib.h> header file. */
-#define HAVE_STDLIB_H 1
-
-/* Define if you have the <strings.h> header file. */
-#define HAVE_STRINGS_H 1
-
-/* Define if you have the <string.h> header file. */
-#define HAVE_STRING_H 1
-
-/* Define if you have the <sys/stat.h> header file. */
-#define HAVE_SYS_STAT_H 1
-
-/* Define if you have the <sys/types.h> header file. */
-#define HAVE_SYS_TYPES_H 1
-
-/* Define if you have the <unistd.h> header file. */
-#define HAVE_UNISTD_H 1
-
-/* Name of package */
-/* #undef PACKAGE */
-
-/* The size of a `char', as computed by sizeof. */
-#define SIZEOF_CHAR 1
-
-/* The size of a `int', as computed by sizeof. */
-#define SIZEOF_INT 4
-
-/* The size of a `long int', as computed by sizeof. */
-#define SIZEOF_LONG_INT 4
-
-/* The size of a `short', as computed by sizeof. */
-#define SIZEOF_SHORT 2
-
-/* Define if you have the ANSI C header files. */
+/* Define if you have the ANSI C header files.  */
 #define STDC_HEADERS 1
 
-/* Version number of package */
-/* #undef VERSION */
-
-/* Define if `lex' declares `yytext' as a `char *' by default, not a `char[]'.
-   */
+/* Define if lex declares yytext as a char * by default, not a char[].  */
 #define YYTEXT_POINTER 1
 
-/* Define to empty if `const' does not conform to ANSI C. */
-/* #undef const */
+/* The number of bytes in a char.  */
+#define SIZEOF_CHAR 1
+
+/* The number of bytes in a int.  */
+#define SIZEOF_INT 4
+
+/* The number of bytes in a long int.  */
+#define SIZEOF_LONG_INT 4
+
+/* The number of bytes in a short.  */
+#define SIZEOF_SHORT 2
+
+/* Define if you have the acosh function.  */
+/* #undef HAVE_ACOSH */
+
+/* Define if you have the asinh function.  */
+/* #undef HAVE_ASINH */
+
+/* Define if you have the atanh function.  */
+/* #undef HAVE_ATANH */
+
+/* Define if you have the <curses.h> header file.  */
+/* #undef HAVE_CURSES_H */
+
+/* Define if you have the <ncurses.h> header file.  */
+#define HAVE_NCURSES_H 1
