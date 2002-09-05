da8fc0188da81a54916905e2de853b12546bdaab
steveman <steveman>
2002-09-05 13:09:23 +0000

    new chars

--- io/win32/htcurses.h
+++ io/win32/htcurses.h
@@ -53,6 +53,8 @@
 #define CHAR_ARROW_DOWN (unsigned char)'\x19'
 #define CHAR_ARROWBIG_UP (unsigned char)'\x1e'
 #define CHAR_ARROWBIG_DOWN (unsigned char)'\x1f'
+#define CHAR_ARROWBIG_RIGHT '>'
+#define CHAR_ARROWBIG_LEFT '<'
 #define CHAR_RADIO	(unsigned char)'\x7'
 
 class screendrawbuf: public genericdrawbuf {
