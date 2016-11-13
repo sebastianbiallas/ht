e6b8ad955d4c82d6002ce0f439cdb957cc6c324d
Sebastian Biallas <sb@biallas.net>
2016-11-13 15:28:47 +0100

    update copyright

--- htinfo.h
+++ htinfo.h
@@ -31,6 +31,6 @@
 
 #define ht_author2 "Sebastian Biallas"
 #define ht_mail2 "sb@biallas.net"
-#define ht_copyright2 "(c) 1999-2015 " ht_author2 " <" ht_mail2 ">"
+#define ht_copyright2 "(c) 1999-2016 " ht_author2 " <" ht_mail2 ">"
 
 #endif /* !__HTINFO_H__ */
